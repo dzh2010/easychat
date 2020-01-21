cbf758eccd3747fb1622e1a46b87fbe016c4f0e3
Denis <den.zhonin@gmail.com>
2020-01-21 11:50:59 +0300

    initial commit

---
+++ src/test/java/ru/rzn/dzh/easychat/EasychatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rzn.dzh.easychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasychatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
