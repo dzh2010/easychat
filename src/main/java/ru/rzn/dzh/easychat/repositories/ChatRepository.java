47917e9ca47373a28ed0383c2cd3834bbd47a698
Denis G. Zhonin <den.zhonin@gmail.com>
2020-01-24 22:34:14 +0300

    add transactional annotation

--- src/main/java/ru/rzn/dzh/easychat/repositories/ChatRepository.java
+++ src/main/java/ru/rzn/dzh/easychat/repositories/ChatRepository.java
@@ -2,8 +2,10 @@ package ru.rzn.dzh.easychat.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.rzn.dzh.easychat.models.Chat;
 
 @Repository
+@Transactional
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 }
--- src/main/java/ru/rzn/dzh/easychat/repositories/MessageRepository.java
+++ src/main/java/ru/rzn/dzh/easychat/repositories/MessageRepository.java
@@ -2,11 +2,13 @@ package ru.rzn.dzh.easychat.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.rzn.dzh.easychat.models.Message;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     List<Message> findByChat_Id(Long chatId);
